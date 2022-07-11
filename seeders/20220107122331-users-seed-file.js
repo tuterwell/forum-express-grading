848feeeba40140973c56b54f49223daa50484f04
zjzheng17 <90173539+zjzheng17@users.noreply.github.com>
2022-07-11 11:31:27 +0800

    Update 20220107122331-users-seed-file.js

--- seeders/20220107122331-users-seed-file.js
+++ seeders/20220107122331-users-seed-file.js
@@ -28,6 +28,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Users', null, {})
+    await queryInterface.bulkDelete('Users', {})
   }
 }
